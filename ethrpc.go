fe8831d129c281cf7277b132a5fc2decdf5a4a83
GuiltyMorishita <morilliantblue@gmail.com>
2017-11-29 17:13:11 +0900

    Add GetBalance, GetTransactionCount SendRawTransaction
     ethrpc.go  |   9 ++++++
     gethrpc.go | 101 -------------------------------------------------------------
     main.go    |   6 ++++
     3 files changed, 15 insertions(+), 101 deletions(-)

---
+++ ethrpc.go
@@ -0,0 +1,63 @@
+package ethrpc
+
+import (
+	"github.com/GuiltyMorishita/jsonrpc"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
+
+type EthRPC struct {
+	RPCClient *jsonrpc.RPCClient
+}
+
+func NewEthRPC(endpoint string) EthRPC {
+	return EthRPC{
+		RPCClient: jsonrpc.NewRPCClient(endpoint),
+	}
+}
+
+func (rpc EthRPC) GetBalance(address, block string) (balance string, err error) {
+	response, err := rpc.RPCClient.Call("eth_getBalance", address, block)
+	if err != nil {
+		return
+	}
+
+	if response.Error != nil {
+		err = response.Error
+		return
+	}
+
+	response.GetObject(&balance)
+	return
+}
+
+func (rpc EthRPC) GetTransactionCount(address, block string) (count uint64, err error) {
+	response, err := rpc.RPCClient.Call("eth_getTransactionCount", address, block)
+	if err != nil {
+		return
+	}
+
+	if response.Error != nil {
+		err = response.Error
+		return
+	}
+
+	var countHex string
+	response.GetObject(&countHex)
+	count, _ = hexutil.DecodeUint64(countHex)
+	return
+}
+
+func (rpc EthRPC) SendRawTransaction(data string) (txHash string, err error) {
+	response, err := rpc.RPCClient.Call("eth_sendRawTransaction", data)
+	if err != nil {
+		return
+	}
+
+	if response.Error != nil {
+		err = response.Error
+		return
+	}
+
+	response.GetObject(&txHash)
+	return
+}
---
+++ ethrpc_test.go
@@ -0,0 +1,109 @@
+package ethrpc
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+	"testing"
+
+	"github.com/AltaApps/daikoku-server/helper"
+	"github.com/GuiltyMorishita/ethutil"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/joho/godotenv"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMain(m *testing.M) {
+	_ = godotenv.Load(".test.env")
+	exitCode := m.Run()
+	defer os.Exit(exitCode)
+}
+
+func WithEthRPC(f func(rpc EthRPC)) func() {
+	return func() {
+		f(NewEthRPC(os.Getenv("GETH_ENDPOINT")))
+	}
+}
+
+func TestGetBalance(t *testing.T) {
+	Convey("WithEthRPC", t, WithEthRPC(func(rpc EthRPC) {
+		Convey("Success", func() {
+			balance, err := rpc.GetBalance("0x8FfCf7674ED27c7949Ceda9a0bD6799fe74aCf47", "latest")
+			So(err, ShouldBeNil)
+			So(balance, ShouldEqual, "0x56bc75e2d63100000") // 100 ETH
+		})
+
+		Convey("Empty Address", func() {
+			_, err := rpc.GetBalance("", "latest")
+			So(err.Error(), ShouldContainSubstring, "hex string has length 0")
+		})
+
+		Convey("Invalid Address", func() {
+			_, err := rpc.GetBalance("InvalidAddress", "latest")
+			So(err.Error(), ShouldContainSubstring, "cannot unmarshal hex string without 0x prefix ")
+		})
+	}))
+}
+
+func TestGetTransactionCount(t *testing.T) {
+	Convey("WithEthRPC", t, WithEthRPC(func(rpc EthRPC) {
+		Convey("Success", func() {
+			count, err := rpc.GetTransactionCount(ethutil.AddressHex(1), "pending")
+			So(err, ShouldBeNil)
+			So(count, ShouldHaveSameTypeAs, uint64(1))
+			So(count, ShouldBeGreaterThan, uint64(0))
+		})
+
+		Convey("Empty Address", func() {
+			_, err := rpc.GetTransactionCount("", "pending")
+			So(err.Error(), ShouldContainSubstring, "hex string has length 0")
+		})
+
+		Convey("Invalid Address", func() {
+			_, err := rpc.GetTransactionCount("InvalidAddress", "pending")
+			So(err.Error(), ShouldContainSubstring, "cannot unmarshal hex string without 0x prefix ")
+		})
+	}))
+}
+
+func TestSendRawTrancastion(t *testing.T) {
+	Convey("WithEthRPC", t, WithEthRPC(func(rpc EthRPC) {
+		Convey("Success", func() {
+			txHash, err := rpc.SendRawTransaction(
+				signedTransactionData(ethutil.PrivateKeyHex(1), ethutil.AddressHex(2)),
+			)
+			So(err, ShouldBeNil)
+			So(txHash, ShouldStartWith, "0x")
+		})
+
+		Convey("Empty Data", func() {
+			_, err := rpc.SendRawTransaction("")
+			So(err.Error(), ShouldContainSubstring, "EOF")
+		})
+
+		Convey("Invalid Data", func() {
+			_, err := rpc.SendRawTransaction("InvalidData")
+			So(err.Error(), ShouldContainSubstring, "cannot unmarshal hex string without 0x prefix ")
+		})
+	}))
+}
+
+func signedTransactionData(privateKeyHex string, toAddressHex string) (transactionData string) {
+	from, _ := helper.PrivateKeyHexToAddressHex(privateKeyHex)
+	nonce, _ := NewEthRPC(os.Getenv("GETH_ENDPOINT")).GetTransactionCount(from, "pending")
+	toAddress := common.HexToAddress(toAddressHex)
+	value := big.NewInt(100000000000000000)
+	gasLimit := big.NewInt(21000)
+	gasPrice := big.NewInt(3000000000)
+	data := []byte{}
+
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+	signer := types.NewEIP155Signer(big.NewInt(15))
+	privateKey, _ := crypto.HexToECDSA(privateKeyHex)
+	signedTx, _ := types.SignTx(tx, signer, privateKey)
+	ts := types.Transactions{signedTx}
+	transactionData = fmt.Sprintf("0x%x", ts.GetRlp(0))
+	return
+}
